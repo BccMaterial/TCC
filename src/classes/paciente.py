fdbb22c362e096595925402c0fe387af0b83e586
thiagopls1 <thiagopls1@hotmail.com>
2026-09-14 19:06:25 -0300

    refactor: add tcc code into repository

---
+++ src/classes/paciente.py
@@ -0,0 +1,27 @@
+import uuid
+
+
+class Paciente:
+    """
+    Representa um paciente para ser utilizado no SimPy
+
+    Os atributos são usados pelo simulador para controlar o fluxo de triagem e avaliações.
+
+    Atributos:
+        id: UUID
+            ID de identificação do paciente
+        suspeita : str
+            Nome do transtorno que o paciente apresenta.
+        num_avaliacoes : int, default=0
+            Número de avaliações já realizadas para esse paciente.
+        triagem_feita : bool, default=False
+            Indica se a triagem foi concluída.
+    """
+
+    def __init__(
+        self, suspeita: str, num_avaliacoes: int = 0, triagem_feita: bool = False
+    ) -> None:
+        self.id: uuid.UUID = uuid.uuid4()
+        self.suspeita: str = suspeita
+        self.num_avaliacoes: int = num_avaliacoes
+        self.triagem_feita: bool = triagem_feita
---
+++ src/main.py
@@ -0,0 +1,37 @@
+import random
+
+import simpy
+
+from classes import Paciente
+
+
+def paciente(env, paciente, profissional):
+    chegada = env.now
+    print(f"{paciente.id} chegou em {chegada:.2f}")
+
+    with profissional.request() as request:
+        yield request
+
+        espera = env.now - chegada
+        print(f"Paciente {paciente.id} começou atendimento após esperar {espera:.2f}")
+        tempo_avaliacao = random.uniform(30, 60)
+
+        yield env.timeout(tempo_avaliacao)
+
+    print(f"Paciente {paciente.id} terminou em {env.now:.2f}")
+
+
+def chegada_pacientes(env, profissional):
+    while True:
+        p = Paciente(suspeita="Autismo")
+        env.process(paciente(env, p, profissional))
+
+        intervalo = random.expovariate(1 / 20)
+        yield env.timeout(intervalo)
+
+
+if __name__ == "__main__":
+    env = simpy.Environment()
+    profissional = simpy.Resource(env, capacity=1)
+    env.process(chegada_pacientes(env, profissional))
+    env.run(until=480)  # 480 minutos = 8 horas
